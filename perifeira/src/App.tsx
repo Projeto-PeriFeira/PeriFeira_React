133a0df654b23585e4e4ade7e409f4eb9b539155
renatonunes74 <renatonunes74@gmail.com>
2023-05-17 10:07:30 -0300

    react

---
+++ perifeira/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Home from './paginas/home/Home'
+
+function App() {
+  return (
+	<>
+	<Home />
+	</>
+  )
+}
+
+export default App;
---
+++ perifeira/src/paginas/home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+	return (
+			<>
+			<h1>teste</h1>
+			</>
+	       );
+};
+export default Home;
