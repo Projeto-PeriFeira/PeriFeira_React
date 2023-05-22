f13d7c85f2128ca2d3e519af8cb5c5b8431a762f
Jessica Mendes <jessicamend17@gmail.com>
2023-05-22 16:26:26 -0300

    Resolução de conflito

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -4,7 +4,7 @@ import { Grid, Box, TextField, Typography, Button} from '@mui/material'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import Usuario from '../../model/Usuario';
-import {cadastroUsuario} from '../../services/Service'
+
 
 function cadastrarUsuario() {
 
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,12 +4,13 @@ export const api = axios.create({
     baseURL: 'https://db-perifeira.onrender.com/swagger-ui/'
 }) 
 
+export const cadastroUsuario = async (url: string, dados: object, setDados: any) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
+
 export const login = async (url: string, dados: object, setDados: any) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data.token)
 }
 
-export const cadastroUsuario = async (url: string, dados: object, setDados: any) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-}
