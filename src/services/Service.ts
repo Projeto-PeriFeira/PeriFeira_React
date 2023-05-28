d2fa5d2a0b18b78581fc188afdc9f97680a1a521
renatonunes74 <renato.s.nunes74@gmail.com>
2023-05-28 19:42:35 -0300

    refactor: Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: ''
+	baseURL: 'https://db-perifeira.onrender.com'
 }) 
-
-// export const login = async (url: string, dados: object, setDados: any) => {
-//     const resposta = await api.post(url, dados)
-//     setDados(resposta.data.token)
-// }
 
 export const login = async (url: any, dados: any, setDados: any) => {
 	const resposta = await api.post(url, dados)
